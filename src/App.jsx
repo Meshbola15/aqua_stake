fb72abd09cb33871fcc6d6bb92e5dbd84d4c3eb1
Meshbola15 <Meshbola15@gmail.com>
2024-03-21 21:59:13 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
 import "./App.css";
 import Home from "./pages/home";
 import StakeHome from "./pages/stake";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+
+const ErrorPage = () => {
+  let navigator = useNavigate();
+  let location = useLocation();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigator("/");
+      console.log(location.pathname);
+    }, 20);
+    return () => clearTimeout(timer);
+  }, []);
+  return <div>Redirecting....</div>;
+};
 
 function App() {
   return (
     <Routes>
-      <Route path="*" element={<Home />} children />
+      <Route path="*" element={<ErrorPage />} />
+      <Route path="/" element={<Home />} children />
       <Route path="/stake/*" element={<StakeHome />} />
     </Routes>
   );

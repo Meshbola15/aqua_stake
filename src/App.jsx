6330b4c18bc4a59c8699b467ae6cebe1ed79e6f9
Olanrewaju Oduola <lanzzd191@gmail.com>
2024-03-21 22:33:43 +0100

    Fix no route error

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Home from "./pages/home";
 import StakeHome from "./pages/stake";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-
   return <div>Redirecting....</div>;
 };
 
 function App() {
-
   const navigator = useNavigate();
   const location = useLocation();
 
-  const checkRoute = () => {
-    if (location.pathname !== "/") {
-      navigator("/")
-      const expectedRoute = location.pathname.slice(1)
-      const timer = setTimeout(() => {
-        navigator(expectedRoute)
-      }, 20);
-      return () => clearTimeout(timer);
-    }
-  }
-
   useEffect(() => {
-    checkRoute()
-  }, []);
+    const checkRoute = () => {
+      if (location.pathname !== "/") {
+        const expectedRoute = location.pathname.slice(1);
+        const timer = setTimeout(() => {
+          navigator(expectedRoute);
+        }, 20);
+        return () => clearTimeout(timer);
+      }
+    };
+
+    const cleanup = checkRoute();
+    return cleanup; 
+  }, [location.pathname, navigator]);
 
   return (
     <Routes>
       <Route path="*" element={<ErrorPage />} />
-      <Route path="/" element={<Home />} children />
+      <Route path="/" element={<Home />} />
       <Route path="/stake/*" element={<StakeHome />} />
     </Routes>
   );
